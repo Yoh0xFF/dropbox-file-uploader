028e15b168b8aa767027d036b0f7f9d0be7c1464
Ioram Gordadze <Yoh0xFF@gmail.com>
2016-04-03 00:01:24 +0400

    tiny fixes

--- src/main/java/util/App.java
+++ src/main/java/util/App.java
@@ -4,6 +4,7 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.CommitInfo;
+import com.dropbox.core.v2.files.DbxUserFilesRequests;
 import com.dropbox.core.v2.files.UploadSessionCursor;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -19,7 +20,7 @@ public class App {
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String CONFIG_FILE_PATH = "/etc/dropbox-file-uploader/app.properties";
     private static final int BUFFER_SIZE = 1024 * 1024 * 100; // 100MB
-    
+
     public static void main(String[] args) {
         Properties config = new Properties();
         try (InputStream in = new FileInputStream(CONFIG_FILE_PATH)) {
@@ -28,20 +29,21 @@ public class App {
             LOGGER.error("Please create configuration file \"/etc/dropbox-file-uploader/app.properties\"");
             return;
         }
-        
+
         String dbxAccessToken = config.getProperty("DBX_ACCESS_TOKEN");
         String dbxClientName = config.getProperty("DBX_CLIENT_NAME");
-        
+
         if (args.length == 0) {
             LOGGER.error("Please pass file path");
             return;
         }
 
+        // Create Dropbox client
+        DbxRequestConfig dbcRequestConfig = new DbxRequestConfig(dbxClientName, "en_US");
+        DbxClientV2 dbxClient = new DbxClientV2(dbcRequestConfig, dbxAccessToken);
+        DbxUserFilesRequests dbxFileMngr = dbxClient.files();
+        
         for (String filePath : args) {
-            // Create Dropbox client
-            DbxRequestConfig dbcRequestConfig = new DbxRequestConfig(dbxClientName, "en_US");
-            DbxClientV2 dbxClient = new DbxClientV2(dbcRequestConfig, dbxAccessToken);
-
             LOGGER.info("Start file upload: {}", filePath);
 
             // Upload file to Dropbox
@@ -49,7 +51,7 @@ public class App {
             try {
                 if (file.length() <= BUFFER_SIZE) {
                     try (InputStream in = new FileInputStream(file)) {
-                        dbxClient.files().uploadBuilder("/" + file.getName()).uploadAndFinish(in);
+                        dbxFileMngr.uploadBuilder("/" + file.getName()).uploadAndFinish(in);
                     }
                 } else {
                     byte[] buffer = new byte[BUFFER_SIZE];
@@ -66,14 +68,14 @@ public class App {
 
                             try (ByteArrayInputStream bis = new ByteArrayInputStream(buffer, 0, k)) {
                                 if (firstRead) {
-                                    sessionId = dbxClient.files().uploadSessionStart().uploadAndFinish(bis).getSessionId();
+                                    sessionId = dbxFileMngr.uploadSessionStart().uploadAndFinish(bis).getSessionId();
                                     firstRead = false;
                                 } else if (lastRead) {
                                     UploadSessionCursor cursor = new UploadSessionCursor(sessionId, offset);
                                     CommitInfo commitInfo = new CommitInfo("/" + file.getName());
-                                    dbxClient.files().uploadSessionFinish(cursor, commitInfo).uploadAndFinish(bis);
+                                    dbxFileMngr.uploadSessionFinish(cursor, commitInfo).uploadAndFinish(bis);
                                 } else {
-                                    dbxClient.files().uploadSessionAppend(sessionId, offset).uploadAndFinish(bis);
+                                    dbxFileMngr.uploadSessionAppend(sessionId, offset).uploadAndFinish(bis);
                                 }
                             }
 
