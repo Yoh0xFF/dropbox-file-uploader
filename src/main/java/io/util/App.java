46279c13ccb149b75a7a34267a3d397c06d08202
Ioram Gordadze <ioramgordadze@fb.com>
2022-06-14 11:50:31 +0100

    Upgrade library versions.

--- src/main/java/io/util/App.java
+++ src/main/java/io/util/App.java
@@ -4,7 +4,6 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
 import com.dropbox.core.v2.files.CommitInfo;
-import com.dropbox.core.v2.files.DbxUserFilesRequests;
 import com.dropbox.core.v2.files.UploadSessionCursor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,13 +21,13 @@ public class App {
   private static final int BUFFER_SIZE = 1024 * 1024 * 100; // 100MB
 
   public static void main(String[] args) {
-    String dbxAccessToken = System.getenv("DBX_ACCESS_TOKEN");
+    var dbxAccessToken = System.getenv("DBX_ACCESS_TOKEN");
     if (isBlank(dbxAccessToken)) {
       logger.error("Please set DBX_ACCESS_TOKEN environment variable");
       return;
     }
 
-    String dbxClientName = System.getenv("DBX_CLIENT_NAME");
+    var dbxClientName = System.getenv("DBX_CLIENT_NAME");
     if (isBlank(dbxClientName)) {
       logger.error("Please set DBX_CLIENT_NAME environment variable");
       return;
@@ -40,17 +39,17 @@ public class App {
     }
 
     // Create Dropbox client
-    DbxRequestConfig dbcRequestConfig = DbxRequestConfig.newBuilder(dbxClientName).withUserLocale("en_US").build();
-    DbxClientV2 dbxClient = new DbxClientV2(dbcRequestConfig, dbxAccessToken);
-    DbxUserFilesRequests dbxFileMngr = dbxClient.files();
+    var dbcRequestConfig = DbxRequestConfig.newBuilder(dbxClientName).withUserLocale("en_US").build();
+    var dbxClient = new DbxClientV2(dbcRequestConfig, dbxAccessToken);
+    var dbxFileMngr = dbxClient.files();
 
     // create folder in dropbox
-    LocalDate curDate = LocalDate.now();
-    String curYear = "" + curDate.getYear();
-    String curMonth = curDate.getMonthValue() < 10 ? "0" + curDate.getMonthValue() : "" + curDate.getMonthValue();
-    String dbxFolder = "/" + curYear + "/" + curMonth;
+    var curDate = LocalDate.now();
+    var curYear = "" + curDate.getYear();
+    var curMonth = curDate.getMonthValue() < 10 ? "0" + curDate.getMonthValue() : "" + curDate.getMonthValue();
+    var dbxFolder = "/" + curYear + "/" + curMonth;
 
-    boolean folderExists = true;
+    var folderExists = true;
     try {
       dbxFileMngr.getMetadata(dbxFolder);
     } catch (DbxException ex) {
@@ -67,24 +66,25 @@ public class App {
     }
 
     // Upload files to Dropbox
-    for (String filePath : args) {
+    for (var filePath : args) {
       logger.info("Start file upload: {}", filePath);
 
-      File file = new File(filePath);
-      String dbxFilePath = dbxFolder + "/" + file.getName();
+      var file = new File(filePath);
+      var dbxFilePath = dbxFolder + "/" + file.getName();
       try {
         if (file.length() <= BUFFER_SIZE) {
           try (InputStream in = new FileInputStream(file)) {
             dbxFileMngr.uploadBuilder(dbxFilePath).uploadAndFinish(in);
           }
         } else {
-          byte[] buffer = new byte[BUFFER_SIZE];
+          var buffer = new byte[BUFFER_SIZE];
           int k;
-          long offset = 0;
-          String sessionId = "";
+          var offset = 0;
+          var sessionId = "";
 
-          try (InputStream in = new FileInputStream(file)) {
-            boolean firstRead = true, lastRead = false;
+          try (var in = new FileInputStream(file)) {
+            var firstRead = true;
+            var lastRead = false;
             while ((k = in.read(buffer)) != -1) {
               if (k < BUFFER_SIZE) {
                 lastRead = true;
@@ -117,11 +117,11 @@ public class App {
   }
 
   private static boolean isBlank(final CharSequence cs) {
-    final int strLen = (cs == null ? 0 : cs.length());
+    final var strLen = (cs == null ? 0 : cs.length());
     if (strLen == 0) {
       return true;
     }
-    for (int i = 0; i < strLen; i++) {
+    for (var i = 0; i < strLen; i++) {
       if (!Character.isWhitespace(cs.charAt(i))) {
         return false;
       }
